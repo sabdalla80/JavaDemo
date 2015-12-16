b20d213dbfa4255ec85995be1c33a358d7e72a79
Samer Abdallah <sabdalla80@hotmail.com>
2015-12-15 22:39:58 -0500

    Added TreeSet example to main

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * 
  */
@@ -14,7 +18,18 @@ public class TestDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world from Samer");
+		
+		Set<String> set = new TreeSet<String>();
+		set.add("sam");
+		set.add("adam");
+		set.add("Zak");
+		set.add(" amani"); //sapce first, UpperCase second, lowercase third
+		
+		for(String s: set)
+			System.out.print(s+ " ");
 
 	}
 
 }
+
+
