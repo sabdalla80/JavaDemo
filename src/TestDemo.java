99154ef3a8d82d1a30138e92cef6ca3395d1dc31
Samer Abdallah <sabdalla80@hotmail.com>
2015-12-15 22:34:30 -0500

    first commit

---
+++ src/TestDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Samer
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world from Samer");
+
+	}
+
+}
